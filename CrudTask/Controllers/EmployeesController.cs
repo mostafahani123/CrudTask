56360dbbbd7be862a38610bfac34574cc5c5b477
engineer mostafa <mostafajhgfd1122gmail.com>
2025-01-11 21:44:39 +0200

    Add project files.

---
+++ CrudTask/Controllers/EmployeesController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Mvc;
+using CrudTask.Models;
+namespace CrudTask.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: Employees
+        TaskEntities db = new TaskEntities();
+        public ActionResult GetEmployees()
+        {
+            var employees = db.Employees.ToList();
+            return View(employees);
+        }
+        [HttpGet]
+        public ActionResult createEmp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult createEmp(Employee emp)
+        {
+            if (emp == null)
+                return RedirectToAction("GetEmployees");
+            db.Employees.Add(emp);
+            db.SaveChanges();
+            return RedirectToAction("GetEmployees");
+
+        }
+        [HttpGet]
+        public ActionResult EditEmp(int id)
+        {
+            var EmpObject = db.Employees.Where(x=>x.id == id).FirstOrDefault(); 
+             return View(EmpObject);
+
+        }
+        [HttpPost]
+        public ActionResult EditEmp(int id, Employee emp)
+        {
+            var EmpObject = db.Employees.Where(x=>x.id == id).FirstOrDefault();
+            EmpObject.name = emp.name;
+            EmpObject.phone = emp.phone;
+            db.SaveChanges();
+            return RedirectToAction("GetEmployees");
+
+
+        }
+        public ActionResult Delete(int id)
+        {
+            var EmpObject = db.Employees.Where(x => x.id == id).FirstOrDefault();
+            db.Employees.Remove(EmpObject);
+            db.SaveChanges();
+            return RedirectToAction("GetEmployees");
+
+
+        }
+
+
+    }
+}
---
+++ CrudTask/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrudTask.MvcApplication" Language="C#" %>
